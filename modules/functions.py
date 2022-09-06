8363b5c20fff741e108878588d9b90cc908d4085
Daniel Törqvist <daniel.tornqvist@fcg.se>
2022-09-06 15:28:47 +0200

    test

---
+++ modules/functions.py
@@ -0,0 +1,6 @@
+def add_twelve(param):
+    return param + 12
+
+
+def add_six(param):
+    return param + 6
---
+++ modules/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from functions import add_twelve 
+
+df = pd.DataFrame(data={
+    'col1' : [1, 2, 3],
+    'col2' : ['dog', 'cat', 'fish']
+})
+df['col1'] = df['col1'].apply(add_twelve)
+
+print(df)
