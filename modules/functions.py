667395eb875d9df4f5e7877246514c150488146a
Daniel Törqvist <daniel.tornqvist@fcg.se>
2022-09-06 16:57:24 +0200

    Function desc. added

--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,25 @@
+"""
+==========
+Functions
+==========
+Explanation: e.g. "where we keep the useful functions"
+"""
+
+
+
 def add_twelve(param):
+    """
+    Function adds twelve to the given input parameters
+
+    Args: 
+        param (float): The input number to add 12 to
+
+    Returns:
+        float the output number
+
+    Notes:
+        A simple yet effective function examplifying functionality
+    """
     return param + 12
 
 
